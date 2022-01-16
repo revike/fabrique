b008c3d3906d7960c5686dc45eb917a18e18e976
revike <revike@ya.ru>
2022-01-16 21:27:41 +0100

    ..

--- main_app/serializers.py
+++ main_app/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer, BooleanField, CharField, ChoiceField
+from rest_framework.serializers import ModelSerializer, BooleanField, CharField
 
 from main_app.models import Survey, Question, OptionAnswer, Answer
 
